c2ca2334ddde53f425d80c94865ee1c9640d5a30
@joseavera <69088516+joseavera@users.noreply.github.com>
2026-04-08 21:35:38 -0600

    Create Operaciones class with Sum and Subtract methods

---
+++ Operaciones.cs
@@ -0,0 +1,18 @@
+public static class Operaciones
+{
+    public static double Sumar(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Restar(double a, double b)
+    {
+        return a - b;
+    }
+
+    // EQUIPO B: Agreguen aquí el método Multiplicar y Dividir
+    
+    // EQUIPO C: Agreguen aquí el método Potencia (Math.Pow)
+    
+    // EQUIPO D: Agreguen aquí el método Raíz Cuadrada
+}
